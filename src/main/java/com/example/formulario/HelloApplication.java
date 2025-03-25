d861393986e3c52b93d8f638d497865a5ecd5c18
Luiz Felipe <luipedro93@gmail.com>
2025-03-24 21:04:00 -0300

    aprimorado

--- src/main/java/com/example/formulario/HelloApplication.java
+++ src/main/java/com/example/formulario/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
-        stage.setTitle("Rodando!");
+        stage.setTitle("Formulário De Contato");
         stage.setScene(scene);
         stage.show();
     }
